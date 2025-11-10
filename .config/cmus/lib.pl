964ad53598787411839dc832a56b47afc331eec4
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-11-10 20:07:02 +0600

    Cmus

--- .config/cmus/lib.pl
+++ .config/cmus/lib.pl
@@ -1,6 +1,27 @@
-/home/himadri/Music/Kaavish - Faasle.flac
-/home/himadri/Music/Kaavish - Kaavish - Chaltay Rahein.flac
-/home/himadri/Music/Kaavish - Kaavish - Dekho.flac
-/home/himadri/Music/Kaavish - Kaavish - Dil Main Meray.flac
-/home/himadri/Music/Kaavish - Kaavish - Piya Dekho Na.flac
-/home/himadri/Music/Kaavish - Tere Pyar Main.flac
+/home/himadri/Music/Miles/MILES - Miles - Chaad Tara (Official Audio).mp3
+/home/himadri/Music/Miles/MILES - Miles - Neela (Official Audio).mp3
+/home/himadri/Music/Miles/MILES - Miles - Shesh Thikana (Official Audio).mp3
+/home/himadri/Music/Miles/MILES - Miles - Piyashi Mon (Official Audio).mp3
+/home/himadri/Music/Miles/MILES - Miles - Bhulbona Tomake (Official Audio).mp3
+/home/himadri/Music/Miles/MILES - Miles - Hridoyheena (Official Audio).mp3
+/home/himadri/Music/Miles/MILES - Miles - Priyotoma (Official Audio).mp3
+/home/himadri/Music/Miles/MILES - Miles-Pahari Meye.mp3
+/home/himadri/Music/Miles/MILES - Miles - Tumi Nai (Official Audio).mp3
+/home/himadri/Music/Miles/MILES - Miles-Kokhono Dekhecho Shopne.mp3
+/home/himadri/Music/Miles/MILES - Priyotoma Megh ｜ Proticchobi ｜ Miles.mp3
+/home/himadri/Music/Hridayam/Hesham Abdul Wahab - Onakka Munthiri (From ＂Hridayam＂).mp3
+/home/himadri/Music/Hridayam/Hesham Abdul Wahab - Puthiyoru Lokam.mp3
+/home/himadri/Music/Hridayam/Hesham Abdul Wahab - Darshana.mp3
+/home/himadri/Music/Hridayam/Hesham Abdul Wahab - Nagumo.mp3
+/home/himadri/Music/Hridayam/Hesham Abdul Wahab - Thathaka Theithare (From ＂Hridayam＂).mp3
+/home/himadri/Music/Hridayam/Hesham Abdul Wahab - Pottu Thotta Pournami.mp3
+/home/himadri/Music/Hridayam/Hesham Abdul Wahab - Nagumo Revival.mp3
+/home/himadri/Music/Hridayam/Hesham Abdul Wahab - Bas Kar Ji.mp3
+/home/himadri/Music/Hridayam/Hesham Abdul Wahab - Minnalkkodi.mp3
+/home/himadri/Music/Hridayam/Hesham Abdul Wahab - Mukilinte.mp3
+/home/himadri/Music/Kaavish/Kaavish - Faasle.flac
+/home/himadri/Music/Kaavish/Kaavish - Kaavish - Chaltay Rahein.flac
+/home/himadri/Music/Kaavish/Kaavish - Kaavish - Dekho.flac
+/home/himadri/Music/Kaavish/Kaavish - Kaavish - Dil Main Meray.flac
+/home/himadri/Music/Kaavish/Kaavish - Kaavish - Piya Dekho Na.flac
+/home/himadri/Music/Kaavish/Kaavish - Tere Pyar Main.flac
